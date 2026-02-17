75e504feddc78ea6c999aef640c196be2cfff73a
root <root@ip-172-31-8-194.ap-south-1.compute.internal>
2026-02-17 01:06:39 +0000

    Initial commit - Flask 2 tier app with HTTPS setup

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, jsonify
+import psycopg2
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
+@app.route("/")
+def home():
+    return "Flask 2-Tier App Running with HTTPS 🚀"
+
+@app.route("/db")
+def check_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT NOW();")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return jsonify({"Database Time": result[0].strftime("%Y-%m-%d %H:%M:%S")})
+
